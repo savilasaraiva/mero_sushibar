85356a564b9770e69c3020e1b6e239834ceffbbf
Savila Saraiva <saraivasvfarias@gmail.com>
2019-04-28 13:16:10 -0300

    Add layout and css

--- js/cardapio.js
+++ js/cardapio.js
@@ -1,6 +1,6 @@
 
 $('button.accordion').click(function () {
-    $(this).parent().find('div.accordion').slideToggle("slow");
+    $('div.accordion').slideToggle("slow");
     var el = $(this);
     if (el.text() == el.data("text-swap")) {
         el.text(el.data("text-original"));
